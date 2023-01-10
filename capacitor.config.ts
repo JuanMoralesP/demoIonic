8de96bd1986473035b23b989a816a9bcf3b31801
JuanMoralesP <juxxybb@gmail.com>
2023-01-10 11:03:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mx.opit.grupo.salinas.red.unica',
+  appName: 'grupo_salinas_red_unica_demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
